369a5149fa9a6d05bd212bd87e3e3d63f2bb70ed
bigzhu <vermiliondun@gmail.com>
2014-06-11 17:50:34 +0800

    加入上传按钮

--- public/chrome/bigmarks.js
+++ public/chrome/bigmarks.js
@@ -1,8 +1,8 @@
 function getBookMarks(){
     chrome.bookmarks.getTree(
-        function(bookmarkTreeNodes) {
+            function(bookmarkTreeNodes) {
             console.log(bookmarkTreeNodes);
-        });
+            });
 }
 function show(id, bookmarks){
     console.log(id);
@@ -13,25 +13,24 @@ function show(id, bookmarks){
 function post(url, data){
     var method = "POST";
     var postData = "Some data";
-    
+
     // You REALLY want async = true.
     // Otherwise, it'll block ALL execution waiting for server response.
     var async = true;
-    
+
     var request = new XMLHttpRequest();
-    
+
     request.open(method, url, async);
-    
+
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     // Or... request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
     // Or... whatever
-    
+
     // Actually sends the request to the server.
     request.send(data);
 }
 
 function add(id, bookmarks){
-    console.log(bookmarks)
     post("http://0.0.0.0:3000/add", JSON.stringify(bookmarks));
 }
 function remove(id, bookmarks){
@@ -45,3 +44,16 @@ chrome.bookmarks.onCreated.addListener(add);
 //只有title和url改变时会触发
 ///chrome.bookmarks.onChanged.addListener(update);
 //chrome.bookmarks.onMoved.addListener(update);
+//上载全部标签
+function upload(){
+    chrome.bookmarks.getTree(
+            function(bookmarkTreeNodes) {
+            console.log(bookmarkTreeNodes);
+            });
+}
+document.getElementById("id_upload").onclick = function() {
+    chrome.bookmarks.getTree(
+            function(bookmarkTreeNodes) {
+            console.log(bookmarkTreeNodes);
+            });
+};
